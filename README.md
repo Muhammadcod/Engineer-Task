00cdd7cb90e6e8f416f0b1ff47d129371f999107
muhammadcod <adebayomuhammad47@gmail.com>
2023-02-15 08:06:34 +0100

    improve layout responsiveness

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # FE-Engineer Task
-I  built this project as part of a job recruitment process
+
+I built this project as part of a job recruitment process
 Project host site can be found here https://boring-swirles-79b164.netlify.app/
+
 ## Available Scripts
 
 Firstly, clone the project and 'cd into the project directory' and run the following command:
@@ -12,5 +14,3 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.\
 You will also see any lint errors in the console.
-
-
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,35 +1,36 @@
 import React from 'react';
-import EmployeeInfo from "./EmployeeInfo";
+import EmployeeInfo from './EmployeeInfo';
 
-const UserList = ({employees}) => {
+const UserList = ({ employees }) => {
   return (
-    <div className='white' style={{
-      display: `block`,
-      overflowX: `auto`,
-      whiteSpace: `nowrap`
-    }}>
+    <div
+      className="white"
+      style={{
+        display: `block`,
+        overflowX: `auto`,
+        whiteSpace: `nowrap`,
+      }}
+    >
       <table className="table">
         <thead className="thead-light">
-        <tr style={{fontWeight: `500`, fontSize: `12px`}}>
-          <th scope="col">
-            <div className="custom-control custom-checkbox" style={{background: `transparent`}}>
-              <input type="checkbox" className="custom-control-input" id="customCheck1"/>
-              <label className="custom-control-label" htmlFor="customCheck1">
-
-              </label>
-            </div>
-          </th>
-          <th scope="col"></th>
-          <th scope="col">NAME</th>
-          <th scope="col">LOCATION</th>
-          <th scope="col">STATUS</th>
-          <th scope="col">ENTRIES</th>
-          <th scope="col">RISK PROFILE</th>
-          <th scope="col"></th>
-        </tr>
+          <tr style={{ fontWeight: `500`, fontSize: `12px` }}>
+            <th scope="col">
+              <div className="custom-control custom-checkbox" style={{ background: `transparent` }}>
+                <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                <label className="custom-control-label" htmlFor="customCheck1"></label>
+              </div>
+            </th>
+            <th scope="col"></th>
+            <th scope="col">NAME</th>
+            <th scope="col">LOCATION</th>
+            <th scope="col">STATUS</th>
+            <th scope="col">ENTRIES</th>
+            <th scope="col">RISK PROFILE</th>
+            <th scope="col"></th>
+          </tr>
         </thead>
-        <tbody className='employee-wrapper'>
-        <EmployeeInfo employees={employees}/>
+        <tbody className="employee-wrapper">
+          {employees?.length ? <EmployeeInfo employees={employees} /> : <div className="text-center">Loading....</div>}
         </tbody>
       </table>
     </div>
